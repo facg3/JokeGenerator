09155519c22c531609d7a98fc86e3499a8a8f2a6
walaamedhat <nicedree@gmail.com>
2017-12-05 13:12:35 +0200

    prototype2

--- src/handler.js
+++ src/handler.js
@@ -1,33 +1,15 @@
 const request = require('request');
 
-const option = {
-  url: '',
-  headers: {
-    'User-Agent': 'request',
-  },
-};
-const homepage = (req, res) => {
-request(option, (err, result, body) => {
-
-    if (err) { return console.log(err); }
-    if (result.statusCode === 200) {
-      res.end(body);
+const getRandomJoke = (callback, user) => {
+  return request('https://icanhazdadjoke.com/slack', (error, response) => {
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      const jokeJSON = JSON.parse(response.body);
+      const joke = jokeJSON.attachments[0].text;
+      console.log(joke);
     }
   });
 };
 
-const getRandomJoke = (callback, user) => {
-  return request('https://icanhazdadjoke.com/slack', (error, response) => {
-    if (error) {
-      console.log('Error:', error)
-    } else {
-      let jokeJSON = JSON.parse(response.body)
-      let joke = jokeJSON.attachments[0].text
-      console.log(joke);
-
-    }
-  })
-}
-
-getRandomJoke()
-module.exports = homepage;
+getRandomJoke();
